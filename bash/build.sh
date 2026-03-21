216f03f702f9a2911f6ffebf73d56692086fb010
aary <aaryadevv8@gmail.com>
2026-03-21 16:51:23 +1100

    Add build script for Bash installation

---
+++ bash/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+: "${DESTDIR:?DESTDIR must be set}"
+: "${PREFIX:=/usr}"
+case "${SOURCE_SHA256:-unset}" in
+  placeholder-*|unset)
+    echo "error: SOURCE_SHA256 is not set or is a placeholder. Refusing to build." >&2
+    exit 1 ;;
+esac
+./configure \
+    --prefix="${PREFIX}" \
+    --without-bash-malloc \
+    --with-installed-readline \
+    --enable-readline \
+    --disable-nls
+make -j"${JOBS:-$(nproc)}"
+make DESTDIR="${DESTDIR}" install
+ln -sf bash "${DESTDIR}${PREFIX}/bin/sh"
